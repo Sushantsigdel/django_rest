58feee1d38aca6a82761bf91f227b5484631ad6c
Sushant Sigdel <susi_csit2077@lict.edu.np>
2024-09-13 12:18:21 +0545

    Update requirements.txt and serializers.py, views.py, and urls.py
    - Add new dependencies: black, certifi, charset-normalizer, coverage, distlib, django-admin-thumbnails, django-session-timeout, filelock, numpy, pandas, pillow, python-dateutil, python-decouple, pytz, requests, six, urllib3, virtualenv
    - Modify fields in CategorySerializer to only include 'name'
    - Add brand and category serializers in ProductSerializer
    - Change CategoryViewSet and BrandViewSet to inherit from ViewSet instead of ModelViewSet
    - Add ProductViewSet to views.py
    - Register ProductViewSet in urls.py
    - Add empty test_models.py file in tests directory

--- src/apps/product/serializers.py
+++ src/apps/product/serializers.py
@@ -6,7 +6,7 @@ from .models import Brand, Category, Product
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = "__all__"
+        fields = ['name']
 
 
 class BrandSerializer(serializers.ModelSerializer):
@@ -17,6 +17,9 @@ class BrandSerializer(serializers.ModelSerializer):
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    brand = BrandSerializer()
+    category = CategorySerializer()
+
     class Meta:
         model = Product
         fields = "__all__"
--- src/urls.py
+++ src/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from src.apps.product.views import CategoryViewSet, BrandViewSet
+from src.apps.product.views import CategoryViewSet, BrandViewSet, ProductViewSet
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 router = DefaultRouter()
 router.register('category', CategoryViewSet),
-router.register('brand', BrandViewSet)
+router.register('brand', BrandViewSet),
+router.register('product', ProductViewSet),
 
 urlpatterns = [
     path('admin/', admin.site.urls),
